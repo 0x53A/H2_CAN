c20e45daff40feddf9c5a7c70c3eb317e8a81b12
Lukas Rieger <code@lukasrieger.com>
2025-07-08 12:31:29 +0200

    changes

--- mcu/src/main.rs
+++ mcu/src/main.rs
@@ -2,9 +2,8 @@
 #![no_main]
 #![feature(never_type)]
 
-use core::{cell::RefCell, fmt::Write, mem};
+use core::fmt::Write;
 
-use critical_section::Mutex;
 use embedded_can::{self, *};
 use embedded_graphics::{
     mono_font::MonoTextStyle,
@@ -16,25 +15,27 @@ use embedded_graphics::{
 use esp_backtrace as _;
 use esp_hal::{
     delay::Delay,
-    gpio::{Event, Input, InputConfig, Io, Level, Output, OutputConfig, Pull},
-    handler,
+    gpio::{Level, Output, OutputConfig},
     i2c::{self, master::I2c},
     main,
     peripherals::{GPIO17, GPIO18, GPIO21, I2C0},
-    ram,
     time::Rate,
-    twai::{self, filter::SingleStandardFilter, *},
+    twai::{
+        self,
+        filter::SingleStandardFilter,
+        *,
+    },
 };
 
 use embedded_graphics::mono_font::ascii::FONT_10X20;
 
-use anyhow::{Result, bail};
+use anyhow::Result;
 use heapless::String;
 use ssd1306::mode::DisplayConfig;
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
- use esp_alloc as _;
+use esp_alloc as _;
 
 // --
 
@@ -46,8 +47,7 @@ type MyDisplay = ssd1306::Ssd1306<
 
 #[main]
 fn main() -> ! {
-
-    esp_alloc::heap_allocator!(size: 32 * 1024 * 10);
+    esp_alloc::heap_allocator!(size: 32 * 1024);
 
     match _main() {
         Ok(_) => loop {},
@@ -57,52 +57,83 @@ fn main() -> ! {
 
 fn _main() -> Result<!> {
     let peripherals = esp_hal::init(esp_hal::Config::default());
+    let delay = esp_hal::delay::Delay::new();
+
+    let mut str: String<100> = heapless::String::new();
 
     let mut display = init_display(
         peripherals.GPIO21,
         peripherals.I2C0,
         peripherals.GPIO17,
         peripherals.GPIO18,
+        &delay,
     )?;
 
-    // let mut led = Output::new(peripherals.GPIO2, Level::Low, OutputConfig::default());
+    let twai_rx_pin = peripherals.GPIO47;
+    let twai_tx_pin = peripherals.GPIO48;
 
-    // let twai_rx_pin = peripherals.GPIO3;
-    // let twai_tx_pin = peripherals.GPIO4;
+    const TWAI_BAUDRATE: twai::BaudRate = BaudRate::B500K;
 
-    // const TWAI_BAUDRATE: twai::BaudRate = BaudRate::B1000K;
+    let mut twai_config = twai::TwaiConfiguration::new(
+        peripherals.TWAI0,
+        twai_rx_pin,
+        twai_tx_pin,
+        TWAI_BAUDRATE,
+        TwaiMode::Normal,
+    );
 
-    // let mut twai_config = twai::TwaiConfiguration::new(
-    //     peripherals.TWAI0,
-    //     twai_rx_pin,
-    //     twai_tx_pin,
-    //     TWAI_BAUDRATE,
-    //     TwaiMode::Normal,
-    // );
+    // todo: test what happens if no filter is set - are all frames received or none?
+    twai_config.set_filter(
+        const { SingleStandardFilter::new(b"xxxxxxxxxxx", b"x", [b"xxxxxxxx", b"xxxxxxxx"]) },
+    );
 
-    // twai_config.set_filter(
-    //     const { SingleStandardFilter::new(b"xxxxxxxxxx0", b"x", [b"xxxxxxxx", b"xxxxxxxx"]) },
-    // );
+    let mut twai = twai_config.start();
 
-    // let mut twai = twai_config.start();
+    write!(&mut str, "Started TWAI")?;
+    set_status(&mut display, &str)?;
 
-    let mut i = 0;
+    let mut last_percentage = -1.0;
 
     loop {
-        let mut str: String<100> = heapless::String::new();
-        write!(&mut str, "Hello Rust{}", i);
+        match twai.receive() {
+            Ok(frame) => {
+                let data = frame.data();
 
-        setStatus(&mut display, &str)?;
+                let frame_id = frame.id();
+                let is_h2_data = match frame_id {
+                    embedded_can::Id::Standard(std_id) => {
+                        let id_val = std_id.as_raw();
+                        matches!(
+                            id_val,
+                            0x300 | 0x308 | 0x310 | 0x318 |  // NEO974A
+                                0x320 | 0x328 | 0x330 | 0x338 |  // NEO983A  
+                                0x340 | 0x348 | 0x350 | 0x358 // NEO986A
+                        )
+                    }
+                    _ => false,
+                };
 
-        i += 1;
-        // match twai.receive() {
-        //     Ok(frame) => {
-        //         let data = frame.data();
-        //     }
-        //     Err(e) => {
-        //         esp_println::dbg!("Error receiving TWAI frame: {:?}", e);
-        //     }
-        // }
+                if is_h2_data && data.len() >= 2 {
+                    let msg0 = u16::from_be_bytes([data[0], data[1]]);
+
+                    let h2_percentage = (msg0 as f32 - 20.0) / 100.0;
+
+                    if last_percentage != h2_percentage {
+                        last_percentage = h2_percentage;
+
+                        str.clear();
+                        write!(&mut str, "H2: {:.4}%", h2_percentage)?;
+                        set_status(&mut display, &str)?;
+
+                        esp_println::println!("H2: {:.4}%", h2_percentage);
+                    }
+                }
+            }
+            Err(::nb::Error::WouldBlock) => {}
+            Err(e) => {
+                esp_println::println!("Error receiving TWAI frame: {:?}", e);
+            }
+        }
     }
 }
 
@@ -111,9 +142,9 @@ fn init_display(
     i2c: I2C0<'static>,
     sda: GPIO17<'static>,
     scl: GPIO18<'static>,
+    delay: &Delay,
 ) -> Result<MyDisplay> {
     esp_println::dbg!("About to initialize the Heltec SSD1306 I2C LED driver");
-    let delay = esp_hal::delay::Delay::new();
 
     let i2c_config = i2c::master::Config::default().with_frequency(Rate::from_khz(400));
 
@@ -131,8 +162,10 @@ fn init_display(
 
     reset.set_high();
 
-    // PinDriver has a Drop implementation that resets the pin, which would turn off the display
-    mem::forget(reset);
+    // Note:
+    //   PinDriver of IDF had a Drop implementation that resets the pin, which would turn off the display
+    //   This seems to no longer be neccessary when using esp-hal.
+    // mem::forget(reset);
 
     let mut display: ssd1306::Ssd1306<
         ssd1306::prelude::I2CInterface<I2c<'_, esp_hal::Blocking>>,
@@ -149,7 +182,7 @@ fn init_display(
         .init()
         .map_err(|e| anyhow::anyhow!("Display error: {:?}", e))?;
 
-    writeText(
+    write_text(
         &mut display,
         "Hello Rust!",
         BinaryColor::Off,
@@ -166,7 +199,7 @@ fn init_display(
     Ok(display)
 }
 
-fn writeText<D>(
+fn write_text<D>(
     display: &mut D,
     text: &str,
     bg: D::Color,
@@ -199,8 +232,8 @@ where
     Ok(())
 }
 
-fn setStatus(display: &mut MyDisplay, text: &str) -> Result<()> {
-    writeText(
+fn set_status(display: &mut MyDisplay, text: &str) -> Result<()> {
+    write_text(
         display,
         &text,
         BinaryColor::Off,
